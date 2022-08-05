6f02314a9634b71923d8336d5bdf5e394a9a9774
Galina Gorbunova <galinagorbunova542@gmail.com>
2022-08-06 00:44:26 +0400

    work_1

---
+++ Work_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+int[] Massif = new int[10];
+
+void GetArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void GoArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int EvenValue(int[] array)
+{
+    int value = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            value++;
+        }
+    }
+    return value;
+}
+
+GetArray(Massif, 100, 1000);
+GoArray(Massif);
+Console.WriteLine();
+
+int value = EvenValue(Massif);
+Console.WriteLine($"Чётных чисел в массиве: {value}");
+
