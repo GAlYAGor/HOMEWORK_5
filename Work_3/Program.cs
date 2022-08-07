e8c38a8fecc433824789e2e972721523bd0ca07d
Galina Gorbunova <galinagorbunova542@gmail.com>
2022-08-07 17:40:53 +0400

    work_3

---
+++ Work_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+
+double[] GetArray = new double[10];
+for (int i = 0; i < GetArray.Length; i++)
+{
+    GetArray[i] = new Random().Next(1, 10);
+
+    Console.Write(GetArray[i] + " ");
+}
+double max = GetArray[0];
+double min = GetArray[0];
+
+for (int i = 1; i < GetArray.Length; i++)
+{
+    if (max < GetArray[i])
+    {
+        max = GetArray[i];
+    }
+    if (min > GetArray[i])
+    {
+        min = GetArray[i];
+    }
+}
+double result = max - min;
+
+Console.WriteLine($"\nразница между между max ({max}) и min ({min}) элементами: {result}");
